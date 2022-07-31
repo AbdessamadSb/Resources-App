658f3b7a6778e24af1acbb69d07449c547a969f2
Abdessamad Sbili <sbiliabdessamad12@gmail.com>
2022-07-31 17:36:26 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,31 @@
-<template></template>
-<script></script>
+<template>
+  <ul></ul>
+</template>
+<script>
+import ResourcesItem from "./components/learning-resources/ResourcesItem.vue";
+
+export default {
+  components: {
+    ResourcesItem,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The Official Vue js documentation.",
+          link: "https://vuejs.org/",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn how to google stuff",
+          link: "https://google.com/",
+        },
+      ],
+    };
+  },
+  components: { ResourcesItem },
+};
+</script>
